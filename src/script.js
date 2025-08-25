78e890647aee3ba48d0676706a0113656371dc66
Taufiq Nur Rahman <61446259+Taufiqrahman571@users.noreply.github.com>
2025-08-25 14:45:48 +0700

    Update email script.js

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 function openGmailWithAccountChooser() {
-  const gmailUrl = 'https://mail.google.com/mail/?fs=1&to=yourmail@gmail.com&su=Hello%20Team&body=I%20would%20like%20to%20get%20in%20touch.&tf=cm';
+  const gmailUrl = 'https://mail.google.com/mail/?fs=1&to=taufiq@sumunarstudio.com&su=Hello%20Team&body=I%20would%20like%20to%20get%20in%20touch.&tf=cm';
   const accountChooserUrl = `https://accounts.google.com/AccountChooser?continue=${encodeURIComponent(gmailUrl)}`;
   window.open(accountChooserUrl, '_blank', 'noopener,noreferrer');
 }
