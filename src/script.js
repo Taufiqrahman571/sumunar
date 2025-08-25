6459bbd0feb8b45d596dda491efda4190063fc0c
Taufiq Nur Rahman <61446259+Taufiqrahman571@users.noreply.github.com>
2025-08-25 15:03:56 +0700

    Update body email

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 function openGmailWithAccountChooser() {
-  const gmailUrl = 'https://mail.google.com/mail/?fs=1&to=taufiq@sumunarstudio.com&su=Hello%20Team&body=I%20would%20like%20to%20get%20in%20touch.&tf=cm';
+  const gmailUrl = 'https://mail.google.com/mail/?fs=1&to=taufiq@sumunarstudio.com&su=New%20Project%20Inquiry&body=Hi%20Sumunar%20Studio,%0D%0A%0D%0AI%20would%20like%20to%20discuss%20a%20new%20project%20with%20you.%0D%0A%0D%0AHere%20are%20some%20details:%0D%0A-%20Project%20Type:%20%5Be.g.%20Website,%20UI%2FUX,%20Branding%5D%0D%0A-%20Timeline:%20%5Be.g.%202%20months%5D%0D%0A-%20Budget:%20%5Be.g.%20$2000%5D%0D%0A%0D%0APlease%20let%20me%20know%20the%20next%20steps.%0D%0A%0D%0AThanks,%0D%0A%5BYour%20Name%5D&tf=cm';
   const accountChooserUrl = `https://accounts.google.com/AccountChooser?continue=${encodeURIComponent(gmailUrl)}`;
   window.open(accountChooserUrl, '_blank', 'noopener,noreferrer');
 }
